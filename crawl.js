fb9e077a509c7216e55953e444662fb22d902761
Ibrahimm <testingacount1426@gmail.com>
2025-03-01 17:53:46 +0700

    Urls Crawler and test suite

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom')
+
 function normalURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath =  `${urlObj.hostname}${urlObj.pathname}`
@@ -7,6 +9,31 @@ function normalURL(urlString) {
   return hostPath
 }
 
+function getURL(htmlBody, baseURL){
+    const urls = [];
+    const dom = new JSDOM(htmlBody)
+    const linkEls = dom.window.document.querySelectorAll('a')
+    for(const linkEl of linkEls){  
+        if(linkEl.href.slice(0, 1) === '/'){
+            try {
+                const urlObj = new URL(`${baseURL}${linkEl.href}`);
+                urls.push(`${urlObj.href}`);
+            } catch (error) {
+                console.log(`invalid url: ${error.message}`)
+            }
+        }else{
+            try {
+                const urlObj = new URL(linkEl.href);
+                urls.push(`${urlObj.href}`);
+            } catch (error) {
+                console.log(`invalid url: ${error.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 module.exports = {
-    normalURL
+    normalURL,
+    getURL
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,21 +1,88 @@
-const { normalURL } = require('./crawl.js')
-const { test, expect } = require('@jest/globals')
+const { normalURL, getURL } = require("./crawl.js");
+const { test, expect } = require("@jest/globals");
 
-test('normalURL cut protocols', ()=> {
-    const input = 'https://example.com/path'
-    const actual = normalURL(input)
-    const expected = 'example.com/path'
-    expect(actual).toEqual(expected)
-})
-test('normalURL cut trailling slash', ()=> {
-    const input = 'https://blog.example.com/path/'
-    const actual = normalURL(input)
-    const expected = 'blog.example.com/path'
-    expect(actual).toEqual(expected)
-})
-test('normalURL capitalize & http', ()=> {
-    const input = 'https://BLOG.example.com/path'
-    const actual = normalURL(input)
-    const expected = 'blog.example.com/path'
-    expect(actual).toEqual(expected)
-})
+test("normalURL cut protocols", () => {
+  const input = "https://example.com/path";
+  const actual = normalURL(input);
+  const expected = "example.com/path";
+  expect(actual).toEqual(expected);
+});
+test("normalURL cut trailling slash", () => {
+  const input = "https://blog.example.com/path/";
+  const actual = normalURL(input);
+  const expected = "blog.example.com/path";
+  expect(actual).toEqual(expected);
+});
+test("normalURL capitalize & http", () => {
+  const input = "https://BLOG.example.com/path";
+  const actual = normalURL(input);
+  const expected = "blog.example.com/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getURL absolute urls", () => {
+  const inputBody = `
+<html>
+    <body>
+        <a href="https://example.com/path/">
+            HTML link
+        </a>
+    </body>
+</html>
+`;
+  const inputBaseURL = "https://example.com/path/";
+  const actual = getURL(inputBody, inputBaseURL);
+  const expected = ["https://example.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURL relative urls", () => {
+  const inputBody = `
+<html>
+    <body>
+        <a href="/path/">
+            HTML link
+        </a>
+    </body>
+</html>
+`;
+  const inputBaseURL = "https://example.com";
+  const actual = getURL(inputBody, inputBaseURL);
+  const expected = ["https://example.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURL both urls", () => {
+  const inputBody = `
+<html>
+    <body>
+        <a href="https://example.com/path1/">
+            HTML link
+        </a>
+        <a href="/path2/">
+            HTML link
+        </a>
+    </body>
+</html>
+`;
+  const inputBaseURL = "https://example.com";
+  const actual = getURL(inputBody, inputBaseURL);
+  const expected = ["https://example.com/path1/", "https://example.com/path2/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURL Invalid urls", () => {
+  const inputBody = `
+<html>
+    <body>
+        <a href="invalid">
+            Invalid HTML link
+        </a>
+    </body>
+</html>
+`;
+  const inputBaseURL = "https://example.com/path/";
+  const actual = getURL(inputBody, inputBaseURL);
+  const expected = [ ];
+  expect(actual).toEqual(expected);
+});
